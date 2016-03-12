28e697f27e3bff8ccd6f875a24b4a12e8935cdea
Alien Lien <alien_lien@htc.com>
2016-03-13 00:18:20 +0800

    Add sort with permutation

--- python/08/count.py
+++ python/08/count.py
@@ -22,8 +22,27 @@ def gen_sort_list(in_list):
     return out
 
 
+def gen_sort_perm(in_list):
+    perm = [0] * len(in_list)
+    cdf = gen_cdf(in_list, max(in_list))
+    for i in range(len(in_list)-1, -1, -1):
+        val = in_list[i]
+        idx = cdf[val] - 1
+        perm[i] = idx
+        cdf[val] -= 1
+    return perm
+
+
+def gen_sort_list_perm(in_list, perm):
+    out = [0] * len(in_list)
+    for i in range(0, len(in_list)):
+        out[perm[i]] = in_list[i]
+    return out
+
+
 if __name__ == '__main__':
     x = [2, 5, 6, 3, 9, 1, 4, 9, 4, 6, 4, 5]
     print(x)
     print(gen_cdf(x, max(x)))
     print(gen_sort_list(x))
+    print(gen_sort_list_perm(x, gen_sort_perm(x)))
